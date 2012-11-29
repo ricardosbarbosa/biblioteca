77242ccea532c7e891f7316ad21adfc1a0e413a5
Ricardo Barbosa <rbrico@gmail.com>
2012-11-29 20:38:25 -0300

    correcao das datas para o banco postgres

--- app/models/aluno.rb
+++ app/models/aluno.rb
@@ -7,7 +7,8 @@ class Aluno < ActiveRecord::Base
   def atrasos?
     if emprestimos.
         where(:data_de_devolucao => nil).
-        where("strftime('%s',date('now')) - strftime('%s', data_de_emprestimo)  > 7*24*60*60").
+        where("date_part('day', age(date 'now'::timestamp, data_de_emprestimo::timestamp) )  > 7").
+        #extract(dow from timestamp 'now')::int
         count > 0
       true
     else

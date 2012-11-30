ee49a4e9c5d2e74d46a3ac2fe83b6d97d90cf49e
Ricardo Barbosa <rbrico@gmail.com>
2012-11-30 20:17:05 -0300

    mais testes, melhoria no seed, melhoria na interface

--- app/models/emprestimo.rb
+++ app/models/emprestimo.rb
@@ -6,21 +6,28 @@ class Emprestimo < ActiveRecord::Base
 
   validates_presence_of :data_de_emprestimo, :data_de_devolucao, :aluno, :livro
   validate :validate_data_de_emprestimoo_no_passado
-  #validate :validate_data_de_devolucao_anterior_a_data_do_emprestimo
-  #validate :validate_livro_nao_disponivel
-  #validate :validate_devolvido_em_anterior_a_data_de_emprestimo
+  validate :validate_data_de_devolucao_anterior_a_data_do_emprestimo
+  validate :validate_data_de_devolucao_deve_ser_de_no_maximo_7_dias
+  validate :validate_livro_nao_disponivel
+  validate :validate_devolvido_em_anterior_a_data_de_emprestimo
 
   def status
     if devolvido_em
-      if devolvido_em - data_de_emprestimo <= 7
+      if devolvido_em < data_de_devolucao
         "ENTREGUE"
       else
         "ENTREGUE COM ATRASO"
       end
-    elsif Time.now.to_date - data_de_emprestimo > 7
+    elsif Time.now.to_date > data_de_devolucao
       "ATRASADO"
     else
       "ALUGADO"
+    end
+  end
+
+  def validate_data_de_devolucao_deve_ser_de_no_maximo_7_dias
+    if data_de_devolucao and data_de_emprestimo
+      errors.add(:data_de_devolucao, "A data de devolução deve ser de no máximo 7 dias após a data de empréstimo.") if (data_de_devolucao - data_de_emprestimo) > 7.days
     end
   end
 
@@ -30,21 +37,21 @@ class Emprestimo < ActiveRecord::Base
     end
   end
 
-  #def validate_data_de_devolucao_anterior_a_data_do_emprestimo
-  #  if data_de_devolucao
-  #    errors.add(:data_de_emprestimo, "A data de devolução não pode ser anterior a data de emprestimo.") if data_de_devolucao < data_de_emprestimo
-  #  end
-  #end
-  #
-  #def validate_devolvido_em_anterior_a_data_de_emprestimo
-  #  if devolvido_em
-  #    errors.add(:devolvido_em, "A data de devolvido em nao pode ser anterior a data de emprestimo.")  if devolvido_em < data_de_emprestimo
-  #  end
-  #end
-  #
-  #def validate_livro_nao_disponivel
-  #  if livro
-  #    errors.add(:livro, "O livro não encontra-se disponível para empréstimo") if !livro.disponivel?
-  #  end
-  #end
+  def validate_data_de_devolucao_anterior_a_data_do_emprestimo
+    if data_de_devolucao and data_de_emprestimo
+      errors.add(:data_de_devolucao, "A data de devolução não pode ser anterior a data de empréstimo.") if data_de_devolucao < data_de_emprestimo
+    end
+  end
+
+  def validate_devolvido_em_anterior_a_data_de_emprestimo
+    if devolvido_em
+      errors.add(:devolvido_em, "A data de devolução não pode ser anterior a data de empréstimo.")  if devolvido_em < data_de_emprestimo
+    end
+  end
+
+  def validate_livro_nao_disponivel
+    if livro
+      errors.add(:livro, "O livro não encontra-se disponível para empréstimo") if !livro.disponivel?
+    end
+  end
 end
--- test/unit/emprestimo_test.rb
+++ test/unit/emprestimo_test.rb
@@ -1,103 +1,199 @@
 require 'test_helper'
 
 class EmprestimoTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
 
-  #validates_presence_of :data_de_emprestimo, :data_de_devolucao, :aluno, :livro
+  fixtures :emprestimos, :livros
 
-  #validate :validate_data_de_emprestimoo_no_passado
-  #validate :validate_data_de_devolucao_anterior_a_data_do_emprestimo
-  #validate :validate_livro_nao_disponivel
-  #validate :validate_devolvido_em_anterior_a_data_de_emprestimo
+  def setup
+    @emp = emprestimos(:emprestimo1)
+    @emp.data_de_emprestimo = 1.day.from_now
+    @emp.data_de_devolucao = 6.day.from_now
+  end
 
-  fixtures :emprestimos
+  def teardown
+    @emp = nil
+  end
+
+  test "tentando salvar um emprestimo para um livro nao disponivel" do
+    @emp = emprestimos(:emprestimo2)
+    @emp.data_de_emprestimo = 0.day.from_now
+    @emp.data_de_devolucao = @emp.data_de_emprestimo + 1.day
+    @emp.livro = livros(:livro3)
+    assert @emp.valid?
+    @emp.save
+
+    emprestimo2 = Emprestimo.new
+    emprestimo2.data_de_emprestimo = 0.day.from_now
+    emprestimo2.data_de_devolucao = emprestimo2.data_de_emprestimo + 1.day
+    emprestimo2.aluno = @emp.aluno
+    emprestimo2.livro = livros(:livro2)
+    assert emprestimo2.valid?
+    emprestimo2.livro = @emp.livro
+    assert emprestimo2.invalid?
+
+    @emp.delete
+    assert emprestimo2.valid?
+  end
+
+  test "devolvido em anterior a data de emprestimo" do
+    @emp.data_de_emprestimo = 0.day.from_now
+
+    @emp.devolvido_em = @emp.data_de_emprestimo - 1.day
+    assert @emp.invalid?
+    assert @emp.errors[:devolvido_em].any?
+    @emp.devolvido_em = @emp.data_de_emprestimo - 1.month
+    assert @emp.invalid?
+    assert @emp.errors[:devolvido_em].any?
+    @emp.devolvido_em = @emp.data_de_emprestimo - 1.year
+    assert @emp.invalid?
+    assert @emp.errors[:devolvido_em].any?
+
+    @emp.devolvido_em = @emp.data_de_emprestimo + 1.day
+    assert @emp.errors[:devolvido_em].none?
+    @emp.devolvido_em = @emp.data_de_emprestimo + 1.month
+    assert @emp.errors[:devolvido_em].none?
+    @emp.devolvido_em = @emp.data_de_emprestimo + 1.day
+    assert @emp.errors[:devolvido_em].none?
+
+  end
+
+  test "data_de_devolucao deve ser de no maximo 7 dias" do
+    @emp.data_de_emprestimo = 0.day.from_now
+
+    @emp.data_de_devolucao = @emp.data_de_emprestimo + 1.day
+    assert @emp.errors[:data_de_devolucao].none?
+    @emp.data_de_devolucao = @emp.data_de_emprestimo + 2.day
+    assert @emp.errors[:data_de_devolucao].none?
+    @emp.data_de_devolucao = @emp.data_de_emprestimo + 3.day
+    assert @emp.errors[:data_de_devolucao].none?
+    @emp.data_de_devolucao = @emp.data_de_emprestimo + 4.day
+    assert @emp.errors[:data_de_devolucao].none?
+    @emp.data_de_devolucao = @emp.data_de_emprestimo + 5.day
+    assert @emp.errors[:data_de_devolucao].none?
+    @emp.data_de_devolucao = @emp.data_de_emprestimo + 6.day
+    assert @emp.errors[:data_de_devolucao].none?
+    @emp.data_de_devolucao = @emp.data_de_emprestimo + 7.day
+    assert @emp.errors[:data_de_devolucao].none?
+
+    @emp.data_de_devolucao = @emp.data_de_emprestimo + 8.day
+    assert @emp.invalid?
+    assert @emp.errors[:data_de_devolucao].any?
+
+  end
+
+  test "a data de devolucao nao deve ser menor que a data de emprestimo " do
+    @emp.data_de_emprestimo= 0.day.from_now
+
+    @emp.data_de_devolucao = @emp.data_de_emprestimo + 1.day
+    assert @emp.errors[:data_de_devolucao].none?
+    @emp.data_de_devolucao = @emp.data_de_emprestimo + 2.day
+    assert @emp.errors[:data_de_devolucao].none?
+    @emp.data_de_devolucao = @emp.data_de_emprestimo + 3.day
+    assert @emp.errors[:data_de_devolucao].none?
+
+    @emp.data_de_emprestimo= 0.day.from_now
+
+    @emp.data_de_devolucao = 1.day.ago
+    assert @emp.invalid?
+    assert @emp.errors[:data_de_devolucao].any?
+    @emp.data_de_devolucao = 2.day.ago
+    assert @emp.invalid?
+    assert @emp.errors[:data_de_devolucao].any?
+    @emp.data_de_devolucao = 3.day.ago
+    assert @emp.invalid?
+    assert @emp.errors[:data_de_devolucao].any?
+    @emp.data_de_devolucao = 4.day.ago
+    assert @emp.invalid?
+    assert @emp.errors[:data_de_devolucao].any?
+    @emp.data_de_devolucao = 5.day.ago
+    assert @emp.invalid?
+    assert @emp.errors[:data_de_devolucao].any?
+    @emp.data_de_devolucao = 6.day.ago
+    assert @emp.invalid?
+    assert @emp.errors[:data_de_devolucao].any?
+
+  end
 
   test "data de emprestimo no passado" do
-    emp = emprestimos(:emprestimo1)
 
-    emp.data_de_emprestimo = 0.day.from_now
-    emp.data_de_devolucao = 6.day.from_now
-    assert emp.valid?
+    @emp.data_de_emprestimo = 0.day.from_now
+    @emp.data_de_devolucao = 6.day.from_now
+    assert @emp.errors[:data_de_emprestimo].none?
 
-    #emp.data_de_emprestimo = 0.day.from_now
-    #assert emp.valid?
+    @emp.data_de_emprestimo = 0.day.from_now
+    assert @emp.errors[:data_de_emprestimo].none?
 
-    emp.data_de_emprestimo = 1.day.ago
-    assert emp.invalid?, emp.data_de_emprestimo.to_s
-    emp.data_de_emprestimo = 1.month.ago
-    assert emp.invalid?, emp.data_de_emprestimo.to_s
-    emp.data_de_emprestimo = 1.year.ago
-    assert emp.invalid?, emp.data_de_emprestimo.to_s
 
-    emp.data_de_emprestimo = 1.day.from_now.to_date
-    assert emp.valid?, emp.data_de_emprestimo.to_s
-    emp.data_de_emprestimo = 1.month.from_now.to_date
-    assert emp.valid?, emp.data_de_emprestimo.to_s
-    emp.data_de_emprestimo = 1.year.from_now.to_date
-    assert emp.valid?, emp.data_de_emprestimo.to_s
+    @emp.data_de_emprestimo = 1.day.ago
+    @emp.data_de_devolucao = @emp.data_de_emprestimo + 1.day
+    assert @emp.invalid?
+    assert @emp.errors[:data_de_emprestimo].any?
+    @emp.data_de_emprestimo = 1.month.ago
+    @emp.data_de_devolucao = @emp.data_de_emprestimo + 1.day
+    assert @emp.invalid?
+    assert @emp.errors[:data_de_emprestimo].any?
+    @emp.data_de_emprestimo = 1.year.ago
+    @emp.data_de_devolucao = @emp.data_de_emprestimo + 1.day
+    assert @emp.invalid?
+    assert @emp.errors[:data_de_emprestimo].any?
 
-    assert emp.errors[:data_de_emprestimo].none?
+    @emp.data_de_emprestimo = 1.day.from_now.to_date
+    @emp.data_de_devolucao = @emp.data_de_emprestimo + 2.day
+    assert @emp.errors[:data_de_emprestimo].none?
+    @emp.data_de_emprestimo = 30.day.from_now.to_date
+    @emp.data_de_devolucao= @emp.data_de_emprestimo + 1.day
+    assert @emp.errors[:data_de_emprestimo].none?
+    @emp.data_de_emprestimo = 360.year.from_now.to_date
+    @emp.data_de_devolucao= @emp.data_de_emprestimo + 1.day
+    assert @emp.errors[:data_de_emprestimo].none?
+
+    assert @emp.errors[:data_de_emprestimo].none?
 
   end
 
   test "data de emprestimo nao pode ser nulo" do
-    emp = emprestimos(:emprestimo1)
 
-    emp.data_de_emprestimo = 0.day.from_now
-    emp.data_de_devolucao = 6.day.from_now
+    @emp.data_de_emprestimo = 0.day.from_now
+    @emp.data_de_devolucao = 6.day.from_now
 
-    assert emp.valid?
-    assert emp.errors[:data_de_emprestimo].none?
+    assert @emp.errors[:data_de_emprestimo].none?
 
-    emp.data_de_emprestimo = nil
+    @emp.data_de_emprestimo = nil
 
-    assert emp.invalid?
-    assert emp.errors[:data_de_emprestimo].any?
+    assert @emp.invalid?
+    assert @emp.errors[:data_de_emprestimo].any?
   end
 
   test "data_de_devolucao nao pode ser nulo" do
-    emp = emprestimos(:emprestimo1)
 
-    emp.data_de_emprestimo = 0.day.from_now
-    emp.data_de_devolucao = 6.day.from_now
+    assert @emp.errors[:data_de_devolucao].none?
 
-    assert emp.valid?
-    assert emp.errors[:data_de_devolucao].none?
+    @emp.data_de_devolucao = nil
 
-    emp.data_de_devolucao = nil
-
-    assert emp.invalid?
-    assert emp.errors[:data_de_devolucao].any?
+    assert @emp.invalid?
+    assert @emp.errors[:data_de_devolucao].any?
   end
 
   test "aluno nao pode ser nulo" do
-    emp = emprestimos(:emprestimo1)
 
-    emp.data_de_emprestimo = 0.day.from_now
-    emp.data_de_devolucao = 6.day.from_now
+    assert @emp.errors[:aluno].none?
 
-    assert emp.valid?
-    assert emp.errors[:aluno].none?
+    @emp.aluno = nil
 
-    emp.aluno = nil
-
-    assert emp.invalid?
-    assert emp.errors[:aluno].any?
+    assert @emp.invalid?
+    assert @emp.errors[:aluno].any?
   end
 
   test "livro nao pode ser nulo" do
-    emp = emprestimos(:emprestimo1)
-    emp.data_de_emprestimo = 0.day.from_now
-    emp.data_de_devolucao = 6.day.from_now
+    @emp.data_de_emprestimo = 0.day.from_now
+    @emp.data_de_devolucao = 6.day.from_now
 
-    assert emp.valid?
-    assert emp.errors[:livro].none?
+    assert @emp.errors[:livro].none?
 
-    emp.livro = nil
+    @emp.livro = nil
 
-    assert emp.invalid?
-    assert emp.errors[:livro].any?
+    assert @emp.invalid?
+    assert @emp.errors[:livro].any?
+
   end
 end
